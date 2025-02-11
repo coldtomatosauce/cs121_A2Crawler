ace716d108efb4915152c3675fbda1e3af96b508
coldtomatosauce <Tiffqnn18@gmail.com>
2025-02-10 19:15:50 -0800

    fix extract_next_links

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+
 # key: word, value: count
 words_dict = {}
 file = open('stop_words_english.txt', 'r')
@@ -25,15 +26,77 @@ info_subdomains_dict = {}
 stat_subdomains_dict = {}
 
 def scraper(url, resp):
-    write_files(url)
+    write_report(url)
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
-def write_files(url):
+def extract_next_links(url, resp):
+    # Implementation required.
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    if resp.status != 200:
+        return []
+
+    # code from https://beautiful-soup-4.readthedocs.io/en/latest/
+    # get text from link
+    soup = BeautifulSoup(resp.raw_response.content, 'lxml')
+    text = soup.get_text()
+
+    # count words
+    total_words = len(tokenize(text))
+    if total_words < 300:  # if low word count, skip
+        return []
+
+    # decide to crawl link
+    all_crawled_links.add(resp.url)
+    write_crawled_link(resp.url) # append crawled link to file
+
+    if total_words > longest_page[1]:
+        longest_page[0] = resp.url
+
+    # extract links
+    links = []
+    for link in soup.find_all('a'):
+        links.append(link.get('href'))
+
+    valid_links = []
+    for link in links:
+        if is_valid(link):
+            valid_links.append(link)
+
+    if len(valid_links) == 0:
+        return []
+
+    links_res = []
+    for link in valid_links:
+        parsed_url = urlparse(link)._replace(fragment="")
+        clean_link = parsed_url.geturl()
+        # if link has already been or will be crawled
+        if clean_link in all_crawled_links or clean_link in all_added_frontier_links:
+            break
+        # if domain and first segment of path is repeated over the threshold
+        if not count_domain_path(parsed_url):
+            break
+        count_subdomain(parsed_url)
+        links_res.append(clean_link)
+        count_words(clean_link)
+
+    return links_res
+
+def write_crawled_link(link):
+    # append crawled link to file
     f = open("crawled_list.txt", "a")
-    f.write(url + '\n')
+    f.write(link + '\n')
     f.close()
 
+def write_report(url):
+    # write to file a report
     report = open("report.txt", "w")
     report.write("number of unique pages: " + str(len(all_crawled_links)) + '\n')
     report.write("\nlongest page: " + longest_page[0] + ", words: " + longest_page[1] + '\n')
@@ -49,69 +112,24 @@ def write_files(url):
     report.write("\nstat subdomains:\n")
     for sub, count in stat_subdomains_dict.items():
         print(f"\t{sub}: {count}")
-
-def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    if resp.status != 200:
-        return []
-    all_crawled_links.add(resp.url)
-    # code from https://beautiful-soup-4.readthedocs.io/en/latest/
-    links = []
-    soup = BeautifulSoup(resp.raw_response.content, 'lxml')
-    for link in soup.find_all('a'):
-        links.append(link.get('href'))
-
-    valid_links = []
-    for link in links:
-        if is_valid(link):
-            valid_links.append(link)
-
-    links_res = []
-    for link in valid_links:
-        parsed_url = urlparse(link)._replace(fragment="")
-        clean_link = parsed_url.geturl()
-        if clean_link in all_crawled_links or clean_link in all_added_frontier_links: # if link has already been crawled
-            break
-        text = soup.get_text()
-        total_words = len(text.split())
-        if total_words < 300: #  if low word count, skip
-            break
-        if not count_domain_path(parsed_url): # if domain and first segment of path is repeated too much, skip
-            break
-
-        # if link will be downloaded
-        if total_words > longest_page[1]:
-            longest_page[0] = clean_link
-
-        count_subdomain(parsed_url)
-        links_res.append(clean_link)
-        count_words(clean_link)
-
-    return links_res
+    report.close()
 
 def count_subdomain(parsed_url):
+    # update counts of subdomains
     subdomain = parsed_url.netloc.lstrip('w.').split('.')[0]
     if re.match(".*(ics.uci.edu)$", parsed_url.netloc):
         if subdomain == "ics":
             return
         update_count(subdomain, ics_subdomains_dict)
-    elif re.match(".*cs.uci.edu$", parsed_url.netloc):
+    elif re.match(".*(cs.uci.edu)$", parsed_url.netloc):
         if subdomain == "cs":
             return
         update_count(subdomain, cs_subdomains_dict)
-    elif re.match(".*information.uci.edu$", parsed_url.netloc):
+    elif re.match(".*(information.uci.edu)$", parsed_url.netloc):
         if subdomain == "information":
             return
         update_count(subdomain, info_subdomains_dict)
-    elif re.match(".*stat.uci.edu$", parsed_url.netloc):
+    elif re.match(".*(stat.uci.edu)$", parsed_url.netloc):
         if subdomain == "stat":
             return
         update_count(subdomain, stat_subdomains_dict)
@@ -123,6 +141,7 @@ def update_count(key, dictionary):
         dictionary[key] = 1
 
 def tokenize(text):
+    # return list of tokens in text
     tokens = set()
     text = text.lower()
     # using regex, replace all non-alphanumeric characters with space
@@ -130,7 +149,6 @@ def tokenize(text):
     for word in text.split():
         if word not in stop_words:
             tokens.add(word)
-
     tokens = list(tokens)
     return tokens
 
